d3a831ca1baf307a43e81ed1f5a2213f8efd12c3
liamlio <liamcroteau@gmail.com>
2020-01-14 21:41:00 -0500

    Update __init__.py

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import bcrypt
+from flask_ngrok import run_with_ngrok
 
 app = Flask(__name__)
+run_with_ngrok(app)
 #Can't import routes above this since it creates circular imports
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
