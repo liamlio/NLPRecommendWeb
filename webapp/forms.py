47b42e4efbb18cc1f44dc59413b33e5feae52fb2
Akul Vohra <38269791+akulvohra@users.noreply.github.com>
2020-01-30 19:17:07 -0800

    Changes to design

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,16 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, IntegerField
+from wtforms import StringField, SubmitField, IntegerField, TextAreaField
 from wtforms.validators import DataRequired, Length, NumberRange
 
 
 class QueryForm(FlaskForm):
-    query = StringField('Query/Abstract', 
+    query = TextAreaField('Query/Abstract',
     validators=[DataRequired(), Length(min=1)])
 
-    top_k= IntegerField('Number of Recommended Papers', 
+    top_k= IntegerField('Number of Recommended Papers',
     validators=[DataRequired(), NumberRange(min=1, max=10)])
 
     submit = SubmitField('Generate Recommendations')
-
-
-
